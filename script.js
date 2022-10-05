4096c7abe324c6ec087f8710f14c66b84eb91399
dvillanueva93 <114841258+dvillanueva93@users.noreply.github.com>
2022-10-04 22:43:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,1 @@
-const lastName = "Lovelace";
-
-const lastLetterOfLastName = lastName[lastName.length - 1];
+const myList = [["Chocolate Bar", 15], ["Meat", 12], ["Pasta", 9], ["Cooking Oil", 6], ["Milk", 3]];
